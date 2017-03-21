f8b4e4ae014d2fe4a49ebf509415e65e907fc6e4
Alex Tan Hong Pin <alextan220990@gmail.com>
2017-03-21 15:27:11 +0800

    Update solution for golang

--- go/06-sum-square-difference.go
+++ go/06-sum-square-difference.go
@@ -1,20 +1,42 @@
 package main
 
 // Answer: 25164150
+// Find the difference between the sum of the squares
+// of the first one hundred natural numbers and the square of the sum.
 
+// real	0m0.279s
+// user	0m0.145s
+// sys	0m0.089s
 import (
 	"fmt"
 	"math"
 )
 
 func main() {
-	sum := 0
-	sumOfSquare := 0
-	for i := 1; i < 101; i++ {
-		sum += i
-		sumOfSquare += int(math.Pow(float64(i), 2))
+	a := make(chan int)
+	b := make(chan int)
+	go func() {
+		a <- sumOfSquare(101)
+	}()
+	go func() {
+		b <- squareOfSum(101)
+	}()
+	fmt.Println(<-b - <-a)
+}
+
+func sumOfSquare(n int) int {
+	count := 0
+	for i := 1; i < n; i++ {
+		fmt.Println("at a")
+		count += int(math.Pow(float64(i), 2))
 	}
-	squareOfSum := int(math.Pow(float64(sum), 2))
-	difference := squareOfSum - sumOfSquare
-	fmt.Println(difference)
+	return count
+}
+func squareOfSum(n int) int {
+	count := 0
+	for i := 1; i < n; i++ {
+		fmt.Println("at b")
+		count += i
+	}
+	return int(math.Pow(float64(count), 2))
 }
---
+++ go/06-sum-square-difference.v2.go
@@ -0,0 +1,46 @@
+package main
+
+// Answer: 25164150
+// Find the difference between the sum of the squares
+// of the first one hundred natural numbers and the square of the sum.
+
+// Function that returns a channel
+// real  0m0.284s
+// user  0m0.151s
+// sys 0m0.066s
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	a := sumOfSquare(100)
+	b := squareOfSum(100)
+	fmt.Println(<-b - <-a)
+}
+
+// function that returns a channel
+func sumOfSquare(n int) <-chan int {
+	c := make(chan int)
+	go func() {
+		count := 0
+		for i := 1; i <= n; i++ {
+			count += int(math.Pow(float64(i), 2))
+		}
+		c <- count
+		close(c)
+	}()
+	return c
+}
+func squareOfSum(n int) <-chan int {
+	c := make(chan int)
+	go func() {
+		count := 0
+		for i := 1; i <= n; i++ {
+			count += i
+		}
+		c <- int(math.Pow(float64(count), 2))
+		close(c)
+	}()
+	return c
+}
